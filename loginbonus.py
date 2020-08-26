edfee9f0a0c56a8dd72fe8a0fb9e4919dd7d5a9b
AtamaokaC <realatamaokac@gmail.com>
2020-08-26 23:11:53 +0900

    loginbonus: modify the setting of date & path from relayscheduler.

--- loginbonus.py
+++ loginbonus.py
@@ -102,15 +102,10 @@ if __name__ == '__main__':
     history_file_path_format = history_dir + history_file_format
     excluded_members_file_path = base_dir + excluded_members_file
 
-    today = date.today()
+    today = date.today() - timedelta(days=1) # actually yesterday
     ADfirst = date(1,1,1) # AD1.1.1 is Monday
     today_id = (today-ADfirst).days
-    thisweek_id = today_id // 7
-    startday = today + timedelta(min_grace)
-    startday += timedelta((7-startday.weekday())%7)
-    date_id = (startday-ADfirst).days
-    week_id = date_id // 7
-    history_file_path = history_file_path_format % week_id
+    history_file_path = history_file_path_format % today_id
 
     if args.list:
         args.reminder = False
